3e4af5048972a0ccf4e19b889c2b44382a632c85
Arup Chowdhury <arup88130@hotmail.com>
2025-02-03 14:58:43 +0530

    CICD-Test-7

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -1,10 +1,10 @@
 # Terraform Block
 terraform {
-  #required_version = "4.17.0"
+  required_version = "1.10.5"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      #version = ">= 2.0" 
+      version = ">= 4.17.0" 
     }
     random = {
       source = "hashicorp/random"
